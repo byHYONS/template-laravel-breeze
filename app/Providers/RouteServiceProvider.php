5eed72c61ff925158721405e92e78fdf8c1dd21b
byHYONS <byhyons@gmail.com>
2024-07-17 21:27:09 +0200

    feat: corette le rotte

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/admin'; // altro modo per definire costanti (define('HOME', '/dashboard');)
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
