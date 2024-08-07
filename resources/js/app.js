e9badb9eb8b786269a299ab02cd4b7c464d19997
byHYONS <byhyons@gmail.com>
2024-08-07 20:00:23 +0200

    feat: aggiunto componente modale

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,5 +6,9 @@ import '~resources/scss/app.scss';
 //? import boostrap:
 import * as bootstrap from 'bootstrap';
 
+//? importo il componente modale:
+import './components/modale';
+
 //? import per le immagini:
 import.meta.glob(['../img/**']);
+
---
+++ resources/js/components/modale.js
@@ -0,0 +1,97 @@
+/* ***********************************
+        * MODALE DELETE
+*********************************** */
+
+const deleteBtn = document.querySelector('.destroy');
+const deleteBtns = document.querySelectorAll('.destroy');
+const closeBtn = document.querySelector('.modale__exit');
+const closeBtns = document.querySelectorAll('.modale__exit');
+const modale = document.querySelector('.delete__modale');
+const overLay = document.querySelector('.screen');
+
+console.log(deleteBtn);
+
+//? per show page:
+if (deleteBtn) {
+
+    deleteBtn.addEventListener('click', function () {
+        modale.classList.remove('holding');
+        document.body.classList.add('no-scroll');
+        overLay.classList.remove('screen');
+    
+    });
+
+}
+
+//? per index page:
+deleteBtns.forEach(Button => {
+    Button.addEventListener('click', function(e){
+        e.preventDefault();
+      
+        const buttonSlug = Button.getAttribute('data-slug');
+        console.log(`Cancella: ${buttonSlug}`);
+
+        const modaleDelete = document.getElementById(`modale-${buttonSlug}`);
+        console.log(modaleDelete);
+
+        if (modaleDelete) {
+            modaleDelete.classList.remove('holding');
+            document.body.classList.add('no-scroll');
+            overLay.classList.remove('screen');
+
+            window.scrollTo(0, 0);
+
+        };
+
+    });
+
+});
+
+
+//? per show:
+
+if (closeBtn) {
+
+    closeBtn.addEventListener('click', function(){
+        console.log('ho cliccato il bottone closed');
+        modale.classList.add('holding');
+        document.body.classList.remove('no-scroll');
+        overLay.classList.add('screen');
+    
+    });
+}
+
+//? index page:
+
+closeBtns.forEach(Button => {
+    Button.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const modale = Button.closest('.delete__modale');
+        modale.classList.add('holding');
+        document.body.classList.remove('no-scroll');
+        overLay.classList.add('screen');
+
+    })
+})
+
+//TODO: iserire in pagina la modale:
+/*
+
+<a href="{{$project->slug}}" class="destroy" data-slug="{{$project->slug}}">
+    <i class="fas fa-trash"></i>
+</a>  
+
+{{--? modale --}}
+<div class="delete__modale holding" id="modale-{{$project->slug}}">
+    <span class="modale__exit">CHIUDI</span>
+    <h4>Sei sicuro di voler cancellare?</h4>
+    <p>La cancellazione è irreversibile</p>
+    <form id="delete-form-{{$project->slug}}" action="{{route('admin.projects.destroy', $project->slug)}}" method="POST">
+        @csrf
+        @method('DELETE')
+        <input class="delete" type="submit" value="Elimina Elemento">
+    </form>
+</div>
+
+*/
